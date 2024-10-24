6cdec7f9264b10cc384daa67051b27a439107cb0
StripesOO7 <54711792+StripesOO7@users.noreply.github.com>
2024-10-24 23:06:29 +0200

    forgot to import the new location_items.json into the pack

--- pythonProject/base_structure.py
+++ pythonProject/base_structure.py
@@ -380,6 +380,7 @@ Archipelago:AddRetrievedHandler("notify launch handler", onNotifyLaunch)
 
 Tracker:AddItems("items/items.json")
 Tracker:AddItems("items/hint_items.json")
+Tracker:AddItems("items/location_items.json")
 Tracker:AddItems("items/labels.json")
 
 -- Items
