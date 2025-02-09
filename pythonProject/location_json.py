04a48797c887d7fcf2a2488b7d430c175d670a08
StripesOO7 <54711792+StripesOO7@users.noreply.github.com>
2025-02-10 00:22:33 +0100

    - fixed ignoring commented out lines for location_mapping

--- pythonProject/location_json.py
+++ pythonProject/location_json.py
@@ -262,13 +262,16 @@ def create_locations(path: str):  # , logic: dict[str, str]):
     with open(path + "/scripts/autotracking/location_mapping.lua") as mapping:
         while inputs := mapping.readline():
             if "]" in inputs:
-                if "--" in inputs and inputs.rindex("--") > inputs.rindex("}"):
-                    inputs = inputs[: inputs.rindex("--")]
-                    read_input.append(inputs.split("="))
-                elif inputs.rindex("}") == inputs.rindex("{") + 1:
-                    pass
-                elif not (inputs.strip()[0:2] == "--" or inputs.strip()[0:2] == "//"):
-                    read_input.append(inputs.split("="))
+                if not (
+                        inputs.strip()[0:2] == "--" or inputs.strip()[0:2] == "//"
+                ):
+                    if "--" in inputs and inputs.rindex("--") > inputs.rindex("}"):
+                        inputs = inputs[: inputs.rindex("--")]
+                        read_input.append(inputs.split("="))
+                    elif inputs.rindex("}") == inputs.rindex("{") + 1:
+                        pass
+                    else:
+                        read_input.append(inputs.split("="))
             else:
                 pass
 
