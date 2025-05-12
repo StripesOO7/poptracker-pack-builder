906f689e6c5dec05321f0f0f0f55f9fbddf26bd2
StripesOO7 <54711792+StripesOO7@users.noreply.github.com>
2025-05-12 14:58:29 +0200

    addes a script to recalculate map positions on a file-by-file basis and based on a ratio calculated by the old and new image

---
+++ pythonProject/rescale_locations.py
@@ -0,0 +1,41 @@
+def key_lookup(json_dict):
+    if "map_locations" in json_dict.keys():
+        json_dict["map_locations"][0]["x"] = math.floor(int(json_dict["map_locations"][0]["x"])* ratio_width)
+        json_dict["map_locations"][0]["y"] = math.floor(int(json_dict["map_locations"][0]["y"]) * ratio_height)
+    if "children" in json_dict.keys():
+        for index, child in enumerate(json_dict["children"]):
+            json_dict["children"][index] = key_lookup(json_dict["children"][index])
+    return json_dict
+
+if __name__ == '__main__':
+    import json
+    import math
+    import tkinter as tk
+    from tkinter import filedialog
+    from PIL import Image
+
+    root = tk.Tk()
+    root.withdraw()
+
+    print("Select the json to be recalculated. first choose directory second the file itself: ")
+    save_file_name = tk.filedialog.askopenfilename()
+    print("Path to location json-file: ", save_file_name)
+    print("Select old image:")
+    old_img = tk.filedialog.askopenfilename()
+    print("Select new image:")
+    new_img = tk.filedialog.askopenfilename()
+
+    old_width, old_height = Image.open(old_img).size
+    new_width, new_height = Image.open(new_img).size
+    ratio_width = new_width/old_width
+    ratio_height = new_height/old_height
+
+
+    with open(f"{save_file_name}") as recalc_json_file:
+        file_list = recalc_json_file.readlines()
+        file_str = "".join(file_list)
+        file_json_list = json.loads(file_str)
+        for json_obj in file_json_list:
+            json_obj = key_lookup(json_obj)
+    with open(f"{save_file_name.replace('.json', '_recalc.json')}", "w") as new_file:
+        new_file.write(json.dumps(file_json_list, indent=4))
