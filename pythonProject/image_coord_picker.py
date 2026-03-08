280aded60bbc1418486191800f6f52ec07fbc25a
StripesOO7 <54711792+StripesOO7@users.noreply.github.com>
2026-03-08 17:17:03 +0100

    load existing locations on map and remove old entries if you place multiple for the same location

--- pythonProject/image_coord_picker.py
+++ pythonProject/image_coord_picker.py
@@ -78,14 +78,16 @@ def save():
                         path.append(index)
                         break # exit loop after finding correct map and replace data
                 break
-            location_section_json[path[0]] = write_json_partially(path[1:], location_section_json[path[0]], new_data[location])
+            location_section_json[path[0]] = write_json_partially(path[1:], location_section_json[path[0]],
+                                                                  new_data[location][0])
         else:
             pass
 
 
 def resize_image(event):
+    print("start run resize image")
     global image, scaling_factor, canvas_id, new_data
-    new_data = {}
+    # new_data = {}
     if event.width / og_img_width < event.height / og_img_height:
         scaling_factor = event.width / og_img_width
     else:
@@ -95,41 +97,59 @@ def resize_image(event):
     image = copy_of_image.resize((new_width, new_height))
     photo = ImageTk.PhotoImage(image)
     canvas.delete(canvas_id)
-    for id in rectangle_id:
-        canvas.delete(id)
-    rectangle_id.clear()
-    canvas_id = canvas.create_image(0, 0, image=photo, anchor="nw")
-    canvas.image = photo #avoid garbage collection
+    # for id in rectangle_id:
+    #     canvas.delete(id)
+    # rectangle_id.clear()
+    canvas.create_image(0, 0, image=photo, anchor="nw")
+    for location in new_data.keys():
+        canvas.delete(new_data[location][1])
+        canvas.delete(new_data[location][2])
+        new_data[location][1] = canvas.create_rectangle(
+            (new_data[location][0]["x"]*scaling_factor) - 5,
+            (new_data[location][0]["y"]*scaling_factor) - 5,
+            (new_data[location][0]["x"]*scaling_factor) + 5,
+            (new_data[location][0]["y"]*scaling_factor) + 5,
+            fill="red"
+        )
+        new_data[location][2] = canvas.create_text(
+            new_data[location][0]["x"]*scaling_factor,
+            new_data[location][0]["y"]*scaling_factor,
+            text=(
+                f"x:{new_data[location][0]['x']}, y: {new_data[location][0]['y']}\n"
+                f"{new_data[location][0]['map']},\n"
+                f"shape:{new_data[location][0]['shape']}\n"
+                f"size:{new_data[location][0]['size']}"
+            )
+        )
+    canvas.image = photo  # avoid garbage collection
+    print("end run resize image")
 
-def callback(event):
+def place_location(event):
     print("clicked at", event.x, event.y)
     print("scaling factor", scaling_factor)
     print("actual image coords", event.x//scaling_factor, event.y//scaling_factor)
-    new_data[frame_location_selection.focus_get().selection_get()] = build_map_dict(
-                    x = int(event.x//scaling_factor),
-                    y = int(event.y//scaling_factor),
-                    map_name = map_json_selected,
-                    size = size_button['values'][size_button.current()],
-                    shape = shape_button['values'][shape_button.current()],
-                )
-    # new_data[window.focus_get().selection_get()] = {
-    #     "x" : event.x // scaling_factor,
-    #     "y" : event.y // scaling_factor,
-    #     "map_name" : map_json_selected,
-    #     "size" : size_button.current(),
-    #     "shape" : shape_button.current()
-    # }
-    print(new_data)
-    # half_of_size = size_button.current()//2
-    shape_id = canvas.create_rectangle(event.x-5, event.y-5, event.x+5, event.y+5, fill="red")
-    rectangle_id.append(shape_id)
+    canvas.delete(new_data[frame_location_selection.focus_get().selection_get()][1])
+    canvas.delete(new_data[frame_location_selection.focus_get().selection_get()][2])
+    canvas.pack()
+    shape_id = canvas.create_rectangle(event.x - 5, event.y - 5, event.x + 5, event.y + 5, fill="red")
     text_id = canvas.create_text(event.x, event.y, text=(
-            f"x:{event.x//scaling_factor}, y: {event.y//scaling_factor}\n"
-            f"{frame_location_selection.focus_get().selection_get()},\n"
-            f"shape:{shape_button['values'][shape_button.current()]}\n"
-            f"size:{size_button['values'][size_button.current()]}")
-            )
-    rectangle_id.append(text_id)
+        f"x:{event.x // scaling_factor}, y: {event.y // scaling_factor}\n"
+        f"location name: {frame_location_selection.focus_get().selection_get()},\n"
+        f"shape:{shape_button['values'][shape_button.current()]}\n"
+        f"size:{size_button['values'][size_button.current()]}")
+                                 )
+    new_data[frame_location_selection.focus_get().selection_get()] = [
+        build_map_dict(
+            x = int(event.x//scaling_factor),
+            y = int(event.y//scaling_factor),
+            map_name = map_json_selected,
+            size = size_button['values'][size_button.current()],
+            shape = shape_button['values'][shape_button.current()],
+        ),
+        shape_id,
+        text_id
+    ]
+    # print(new_data)
 
 
 def update_coords(x, y):
@@ -157,10 +177,40 @@ def traverse_json(region, path, location_list):
     new_path = f"{path}/{region['name']}"
     if "sections" in region.keys():
         location_list.append(new_path[1:])
-        return
+        # return
     if "children" in region.keys():
         for child in region["children"]:
             traverse_json(child, new_path, location_list)
+    if "map_locations" in region.keys():
+        for map in region["map_locations"]:
+            if map["map"] == map_json_selected:
+                new_data[new_path[1:]] = [
+                    build_map_dict(
+                        x=int(map['x']),
+                        y=int(map['y']),
+                        map_name=map['map'],
+                        size=map['size'] if 'size' in map.keys() else 10,
+                        shape=map['shape'] if 'shape' in map.keys() else 'rect',
+                    ),
+                    canvas.create_rectangle(
+                        (map["x"]*scaling_factor) - 5,
+                        (map["y"]*scaling_factor) - 5,
+                        (map["x"]*scaling_factor) + 5,
+                        (map["y"]*scaling_factor) + 5,
+                        fill="red"
+                    ),
+                    canvas.create_text(
+                        map["x"]*scaling_factor,
+                        map["y"]*scaling_factor,
+                        text=(
+                            f"x:{map['x']}, y: {map['y']}\n"
+                            f"location name: {new_path[1:]},\n"
+                            f"shape:{map['size'] if 'size' in map.keys() else 10}\n"
+                            f"size:{map['shape'] if 'shape' in map.keys() else 'rect'}"
+                        )
+                    )
+                ]
+                print("placed as square")
 
 
 
@@ -189,6 +239,7 @@ if __name__ == "__main__":
     # Maps side
     scrollbar_maps = tk.Scrollbar(frame_map_selection, orient="vertical")
     window_list_of_maps = tk.Listbox(frame_map_selection, name="maps", yscrollcommand=scrollbar_maps.set)
+                                     # , exportselection=False)
     scrollbar_maps.config(command=window_list_of_maps.yview)
     header_maps = tk.Label(frame_map_selection, text="Select a map")
 
@@ -201,6 +252,7 @@ if __name__ == "__main__":
     # location side
     scrollbar_locations = tk.Scrollbar(frame_location_selection, orient="vertical")
     window_list_of_locations = tk.Listbox(frame_location_selection, name="json", yscrollcommand=scrollbar_locations.set)
+                                          # , exportselection=False)
     scrollbar_locations.config(command=window_list_of_locations.yview)
     btn_locations = tk.Button(frame_location_selection, text='select JSON', command=dialog)
     header_locations = tk.Label(frame_location_selection, text="Select location source")
@@ -289,6 +341,17 @@ if __name__ == "__main__":
         copy_of_image = image.copy()
         img = ImageTk.PhotoImage(image=image, name="map image")
 
+
+        canvas = tk.Canvas(frame_map_image, name="map image test", width=img.width(), height=img.height())
+
+
+        location_section_json = json.load(open(f'{base_path}/locations/{locations_json_selected}'))
+        location_list = []
+        for region in location_section_json:
+            path = ""
+            print(region["name"])
+            traverse_json(region, path, location_list)
+
         # window.resizable(True, True)
         canvas = tk.Canvas(frame_map_image, name="map image test", width=img.width(), height=img.height())
         canvas_id = canvas.create_image(0, 0, image=img, anchor="nw")
@@ -298,23 +361,15 @@ if __name__ == "__main__":
         # canvas.bind("<Button-1>", callback)
         # canvas.bind("<MouseWheel>", resize_image)
         # canvas.bind("<B1-Motion>", callback)
-        canvas.bind("<ButtonRelease-1>", callback)
-
-        location_section_json = json.load(open(f'{base_path}/locations/{locations_json_selected}'))
-
-
-        location_list = []
-        for region in location_section_json:
-            path = ""
-            print(region["name"])
-            traverse_json(region, path, location_list)
+        canvas.bind("<ButtonRelease-1>", place_location)
 
 
 
         # btn_map = tk.Button(frame, text='Select Location', command=select_location)
         scrollbar_location_section_y = tk.Scrollbar(frame_location_selection, orient="vertical")
         location_section_list = tk.Listbox(frame_location_selection, name="locations",
-                                           yscrollcommand=scrollbar_location_section_y.set)
+                                           yscrollcommand=scrollbar_location_section_y.set)#, exportselection=False)
+        # location_section_list.bind("<<ListboxSelect>>", callback)
         scrollbar_location_section_y.config(command=window_list_of_locations.yview)
 
         scrollbar_canvas_y = tk.Scrollbar(frame_map_image, orient="vertical")
